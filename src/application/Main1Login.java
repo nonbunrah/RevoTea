2181c1ae13d3f547410977649a5eba64a7808a3c
nonbunrah <rj.bamrah@gmail.com>
2021-10-22 16:08:57 -0700

    login page has been styled

--- src/application/Main1Login.java
+++ src/application/Main1Login.java
@@ -10,6 +10,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
@@ -31,32 +32,44 @@ public class Main1Login extends Application {
 		try {
 			
 			VBox root = new VBox();
-			Scene scene = new Scene(root,500,400);
+			Scene scene = new Scene(root,350,500);
 			StackPane titleBackground = new StackPane();
-			VBox rootMinusTitle = new VBox(5);
+			VBox fields = new VBox(5);
+			VBox buttons = new VBox(5);
 	
 			Text title = new Text("RevoTea");
 			title.setFont(new Font("Arial", 48));
 			title.setStyle("-fx-background-color: #d6d6d6" + "-fx-font-weight: bold");
 			titleBackground.setStyle("-fx-background-color: lightgray");
-			rootMinusTitle.setAlignment(Pos.TOP_CENTER);
-			rootMinusTitle.setPadding(new Insets(20, 0, 0, 0));
+			root.setPadding(new Insets(20, 0, 0, 0));
+			fields.setAlignment(Pos.CENTER);
+			fields.setPadding(new Insets(100, 0, 0, 0));
+			buttons.setAlignment(Pos.CENTER);
+			buttons.setPadding(new Insets(50, 0, 0, 0));
 			
 			titleBackground.getChildren().add(title);
 			
 			TextField userField = new TextField();
 			userField.setPromptText("Username");
+			userField.setMaxSize(250,50);
+			userField.setFont(Font.font("Arial", FontWeight.NORMAL, 20));
 			
 			TextField passField = new TextField();
 			passField.setPromptText("Password");
+			passField.setMaxSize(250,50);
+			passField.setFont(Font.font("Arial", FontWeight.NORMAL, 20));
 			
-			Button login = createButton("login", 70, 30);
-			Button signup = createButton("signup", 70, 30);
-			Button checkout = createButton("checkout", 70, 30);
+			Button login = createButton("Login", 140, 30);
+			Button signup = createButton("Sign Up", 140, 30);
+			Button checkout = createButton("Checkout as Guest", 140, 30);
+			login.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+			signup.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+			checkout.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
 		
 			root.setStyle("-fx-background-color: #f8e192");
-			root.getChildren().addAll(titleBackground, rootMinusTitle);
-			rootMinusTitle.getChildren().addAll(userField, passField, login, signup, checkout);
+			root.getChildren().addAll(titleBackground, fields, buttons);
+			buttons.getChildren().addAll(login, signup, checkout);
+			fields.getChildren().addAll(userField, passField);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
