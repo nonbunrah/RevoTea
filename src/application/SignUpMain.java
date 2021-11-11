f94d5ef1c2bd6ac267cf6ea5309ffa51fd13c1dc
4tiennguyen <34051678+4tiennguyen@users.noreply.github.com>
2021-11-10 20:26:40 -0800

    Add files via upload
    change file name, change background color, check valid email

---
+++ src/application/SignUpMain.java
@@ -0,0 +1,216 @@
+package application;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+public class SignUpMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Sign Up");
+
+        // Create the registration form grid pane
+        GridPane gridPane = createRegistrationFormPane();
+        // Add UI controls to the registration form grid pane
+        addUIControls(gridPane);
+        // Create a scene with registration form grid pane as the root node
+        Scene scene = new Scene(gridPane, 500, 800);
+        // Set the scene in primary stage
+     // set styling and background color for nodes
+     	Text pageTitle = new Text("RevoTea");
+     	pageTitle.setFont(new Font("Arial", 48));
+     	//titleBackground.setStyle("-fx-background-color: lightgray");
+        primaryStage.setScene(scene);
+        StackPane titleBackground = new StackPane();
+        titleBackground.getChildren().add(pageTitle);
+        gridPane.setStyle("-fx-background-color: #f8e192");
+        
+        
+        primaryStage.show();
+    }
+
+
+    private GridPane createRegistrationFormPane() {
+        // Instantiate a new Grid Pane
+        GridPane gridPane = new GridPane();
+
+        // Position the pane at the center of the screen, both vertically and horizontally
+        gridPane.setAlignment(Pos.CENTER);
+
+        // Set a padding of 20px on each side
+        gridPane.setPadding(new Insets(40, 40, 40, 40));
+
+        // Set the horizontal gap between columns
+        gridPane.setHgap(10);
+
+        // Set the vertical gap between rows
+        gridPane.setVgap(10);
+
+        // Add Column Constraints
+
+        // columnOneConstraints will be applied to all the nodes placed in column one.
+        ColumnConstraints columnOneConstraints = new ColumnConstraints(100, 100, Double.MAX_VALUE);
+        columnOneConstraints.setHalignment(HPos.RIGHT);
+
+        // columnTwoConstraints will be applied to all the nodes placed in column two.
+        ColumnConstraints columnTwoConstrains = new ColumnConstraints(200,200, Double.MAX_VALUE);
+        columnTwoConstrains.setHgrow(Priority.ALWAYS);
+
+        gridPane.getColumnConstraints().addAll(columnOneConstraints, columnTwoConstrains);
+
+        return gridPane;
+    }
+
+    private void addUIControls(GridPane gridPane) {
+        // Add Header
+        Label headerLabel = new Label("Sign Up");
+        headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        gridPane.add(headerLabel, 0,0,2,1);
+        GridPane.setHalignment(headerLabel, HPos.CENTER);
+        GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
+
+        // Add Name Label
+        Label nameLabel = new Label("Full Name : ");
+        gridPane.add(nameLabel, 0,1);
+
+        // Add Name Text Field
+        TextField nameField = new TextField();
+        nameField.setPrefHeight(40);
+        gridPane.add(nameField, 1,1);
+
+
+        // Add Email Label
+        Label emailLabel = new Label("Email ID : ");
+        gridPane.add(emailLabel, 0, 2);
+
+        // Add Email Text Field
+        TextField emailField = new TextField();
+        emailField.setPrefHeight(40);
+        gridPane.add(emailField, 1, 2);
+        
+        // Add username Label
+        Label userLabel = new Label(" Username: ");
+        gridPane.add(userLabel, 0, 3);
+
+        // Add Email Text Field
+        TextField userField = new TextField();
+        userField.setPrefHeight(40);
+        gridPane.add(userField, 1, 3);
+
+        // Add Password Label
+        Label passwordLabel = new Label("Password : ");
+        gridPane.add(passwordLabel, 0, 4);
+
+        // Add Password Field
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPrefHeight(40);
+        gridPane.add(passwordField, 1, 4);
+
+        // Add Submit Button
+        Button submitButton = new Button("Submit");
+        submitButton.setPrefHeight(40);
+        submitButton.setDefaultButton(true);
+        submitButton.setPrefWidth(100);
+        gridPane.add(submitButton, 0, 5, 2, 1);
+        GridPane.setHalignment(submitButton, HPos.CENTER);
+        GridPane.setMargin(submitButton, new Insets(20, 0,20,0));
+
+        submitButton.setOnAction(new EventHandler<ActionEvent>()  {
+            @Override
+            public void handle(ActionEvent event){
+                if(nameField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter your name");
+                    return;
+                }
+                if(emailField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter your email");
+                    return;
+                }
+                else {
+                	
+                }
+                if(passwordField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter a password");
+                    return;
+                }
+                if (isValid(emailField.getText())== false) {
+                	 showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Invalid email"," Invalid email!!!!\nPlease choose another one");
+                	 return;
+                	}
+                
+                
+                //FileWriter myWriter = new FileWriter("info.txt");
+                //myWriter.write(nameField.getText(),",",emailField.getText(),",",passwordField.getText());
+                else {      
+                	//System.out.println(userField.getText());
+                	 try {
+             			inserNewCustomer(userField.getText(),emailField.getText() , passwordField.getText());
+             		} catch (IOException e) {
+             			// TODO Auto-generated catch block
+             			e.printStackTrace();
+             		}
+                	showAlert(Alert.AlertType.CONFIRMATION, gridPane.getScene().getWindow(), "Registration Successful!", "Welcome " + nameField.getText());
+            }
+                        
+            }
+        });
+       
+
+    }
+
+    private void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
+    // Check if email is valid
+    public static boolean isValid(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                            "[a-zA-Z0-9_+&*-]+)*@" +
+                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                            "A-Z]{2,7}$";
+                              
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+    public static void inserNewCustomer(String username, String email, String password) throws IOException 
+    {
+        String textToAppend = username + ',' + email + ',' + password;
+        File f = new File("customerInfo.txt");
+            //Set true for append mode
+        BufferedWriter writer = new BufferedWriter(new FileWriter(f.getAbsolutePath(), true));    
+        writer.write(textToAppend);
+        System.out.println();
+        System.out.println(textToAppend);
+        writer.close();
+    }
+  
+    public static void main(String[] args)  { 
+    	 
+        launch(args);
+        
+    }
+}
